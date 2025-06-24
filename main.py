7ee57b19b455e1585d6d2abd79b131e050db0b9e
MooseTrakz <mgromosky@gmail.com>
2025-06-24 15:09:52 +0000

    added word count to main.py

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = count_words(text)
+    print (f"{num_words} words found in the document")
+
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_words(text):
+    split_text = text.split()
+    word_count = len(split_text)
+    return word_count
 
 
 main()
