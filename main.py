6ab1041eec7a2419c739379daca8c9672c78d2d4
MooseTrakz <mgromosky@gmail.com>
2025-06-30 02:39:41 +0000

    added more text to the output of main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
-from stats import get_num_words
+from stats import get_num_words, get_num_char_sym
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print (f"{num_words} words found in the document")
-
+    print ("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print ("----------- Word Count ----------")
+    print (f"Found {num_words} total words")
+    print ("--------- Character Count -------")
+    print (get_num_char_sym(text))
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -2,3 +2,16 @@ def get_num_words(text):
     split_text = text.split()
     word_count = len(split_text)
     return word_count
+
+def get_num_char_sym(text):
+    lower_case_text = text.lower()
+    char_count = {}
+    for char in lower_case_text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+def sorted_dict(char_count):
+    char_count.sort()
